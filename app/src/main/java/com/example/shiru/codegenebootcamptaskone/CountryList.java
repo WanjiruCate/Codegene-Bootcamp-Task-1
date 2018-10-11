cca6198a9352158b7d4f80f72762cd065a41b7e4
kateshiru <catherinemunene802@gmail.com>
2018-10-11 08:07:52 +0300

    Created CountryList Activity and Linked it with MainActivity

---
+++ app/src/main/java/com/example/shiru/codegenebootcamptaskone/CountryList.java
@@ -0,0 +1,19 @@
+package com.example.shiru.codegenebootcamptaskone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CountryList extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+
+    }
+}
--- app/src/main/java/com/example/shiru/codegenebootcamptaskone/MainActivity.java
+++ app/src/main/java/com/example/shiru/codegenebootcamptaskone/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.shiru.codegenebootcamptaskone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnNext);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CountryList.class));
+            }
+        });
+
+
+
     }
 }
